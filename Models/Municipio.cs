26212167d9e3d45ba461e638cc750a9a9c5095e8
feeelibread <feeelibread@outlook.com>
2025-02-09 22:02:24 -0300

    Criado as outras classes para representar outras tabelas do banco

---
+++ Models/Municipio.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CnpjMailingApi.Models
+{
+    public class Municipio
+    {
+        public string? Codigo { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
---
+++ Models/Simples.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CnpjMailingApi
+{
+    public class Simples
+    {
+        public string? CnpjBasico { get; set; }
+        public string? OpcaoMei { get; set; }
+    }
+}
