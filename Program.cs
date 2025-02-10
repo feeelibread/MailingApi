072990d2f7ccbd1b5a9932c1b02b1e8c0df1e38b
feeelibread <feeelibread@outlook.com>
2025-02-09 22:38:42 -0300

    Configurando o Swagger para realizar as requisições

--- Program.cs
+++ Program.cs
@@ -1,17 +1,54 @@
+using CnpjMailingApi.Data;
+using CnpjMailingApi.Repos;
+using CnpjMailingApi.Services;
+using Microsoft.OpenApi.Models;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+// Registra o DbConnectionFactory com a string de conexão
+builder.Services.AddSingleton<DbConnectionFactory>(new DbConnectionFactory(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CNPJ Search API",
+        Version = "v1",
+        Description = "Api para consultar CNPJs com base em filtros como cidades, CNAEs, município, etc."
+    });
+});
+
+builder.Services.AddScoped<ICnpjMailingRepository, CnpjMailingRepository>();
+builder.Services.AddScoped<CnpjMailingService>();
+
 
 var app = builder.Build();
 
+try
+{
+    await using var connection = new NpgsqlConnection(connectionString);
+    await connection.OpenAsync();
+    Console.WriteLine("Conexão bem-sucedida!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao conectar ao banco de dados: {ex.Message}");
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CNPJ Search API v1");
+    });
+
 }
 
 app.UseHttpsRedirection();
